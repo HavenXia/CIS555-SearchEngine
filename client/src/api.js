0e2d0acb4ec109fb5fd17386e25d1bedc890497f
Haoyu Xia <55463354+HavenXia@users.noreply.github.com>
2021-12-14 11:31:15 -0500

    update api

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 // get the user information
 async function getResults(input) {
   try {
-    const res = await axios.get(`http://ec2-100-27-8-149.compute-1.amazonaws.com:45555/search/${input}`, { crossdomain: true });
+    const res = await axios.get(`http://ec2-52-91-243-63.compute-1.amazonaws.com:45555/search/${input}`, { crossdomain: true });
     console.log(res);
     return res.data;
   } catch (err) {
--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// enable cross-origin
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + './client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
