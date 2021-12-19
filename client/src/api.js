5cc3228303bde195c7d7e22258e56f97d3a2598b
Haoyu Xia <55463354+HavenXia@users.noreply.github.com>
2021-12-18 20:33:02 -0500

    update ec2 api

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 // get the user information
 async function getResults(input) {
   try {
-    const res = await axios.get(`http://ec2-52-91-243-63.compute-1.amazonaws.com:45555/search/${input}`, { crossdomain: true });
+    const res = await axios.get(`http://ec2-54-88-238-34.compute-1.amazonaws.com:45555/search/${input}`, { crossdomain: true });
     console.log(res);
     return res.data;
   } catch (err) {
